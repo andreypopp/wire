2b93415be011a9466dcb062190754722dc055095
Andrey Popp <8mayday@gmail.com>
2012-07-03 22:46:02 +0400

    refactor modules

--- test/lib/runner.js
+++ test/lib/runner.js
@@ -1,8 +1,8 @@
 define([
   'tests/wire/base',
   'tests/wire/track',
-  'tests/wire/Event',
-  'tests/wire/View',
+  'tests/wire/event',
+  'tests/wire/view',
   'tests/wire/template',
   'tests/wire/templateexpression'
   ], function() {});
